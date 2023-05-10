6271e5e2336926f837b6af0c23cf65d202700945
Almurat98 <almurat908@gmail.com>
2023-05-10 18:22:21 -0500

    some change

--- src/main/java/com/glt/repository/UserAccountRepository.java
+++ src/main/java/com/glt/repository/UserAccountRepository.java
@@ -3,6 +3,7 @@ package com.glt.repository;
 import com.glt.entity.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,16 +36,16 @@ public interface UserAccountRepository extends JpaRepository<UserAccount,Long> {
     //Write a native query that returns all users that contain a specific name?
     @Query(value = "SELECT * FROM user_account ua join account_details ad " +
             "On ua.account_details_id = ad.id WHERE ad.name ILIKE concat('%',?!,'%')",nativeQuery = true)
-    List<UserAccount>getUserContainsName(String name);
+    List<UserAccount>getUserContainsName(@Param("name") String name);
     //Write a native query that returns all users?
     @Query(value = "SELECT * FROM user_account",nativeQuery = true)
     List<UserAccount>retrieveAllUser();
     //Write a native query that returns all users in the range of ages?
     @Query(value = "SELECT * FROM user_account ua join account_details ad " +
             "ON ua.account_details_id = ad.id WHERE ad.age BETWEEN ?1 AND ?2",nativeQuery = true)
-    List<UserAccount>getAllUserAgesBetween(Integer age1, Integer age2);
+    List<UserAccount>getAllUserAgesBetween(@Param("age1") Integer age1, @Param("age2")Integer age2);
     //Write a native query to read a user by email?
     @Query(value = "SELECT * FROM user_account ua WHERE ua.email =?1",nativeQuery = true)
-    List<UserAccount>retrieveUserByEmail(String email);
+    List<UserAccount>retrieveUserByEmail(@Param("email") String email);
 
 }
